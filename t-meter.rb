c726fe4ebcc3d7f61dcfd3f3c5f65d2091565bfb
jordan <dev.jordanempire@gmail.com>
2025-11-21 13:06:43 +0300

    Update t-meter to v0.3.1

--- t-meter.rb
+++ t-meter.rb
@@ -1,13 +1,34 @@
 class TMeter < Formula
   desc "A terminal-based day progress bar showing elapsed time and user markers"
   homepage "https://github.com/jordan-jakisa/t-meter"
-  url "https://github.com/jordan-jakisa/t-meter/releases/download/v0.3.0/t-meter-0.3.0-x86_64-apple-darwin.tar.gz"
-  sha256 "8bfad290bdac5a73102aed09428785fc52a1ce8f11d7f8274984c82180732c97"
-  version "0.3.0"
+  url "https://github.com/jordan-jakisa/t-meter/releases/download/v0.3.1/t-meter-0.3.1-x86_64-apple-darwin.tar.gz"
+  sha256 "5fa3345280f02db9311078aa8c2bed9501f5671b79a7248a1def4511d7330647"
+  version "0.3.1"
   license "MIT"
 
   def install
     bin.install "t-meter"
+  end
+
+  def post_install
+    ohai "Configuration"
+    puts <<~EOS
+      t-meter will automatically generate a configuration file on first run at:
+        ~/.config/t-meter/config.toml
+
+      The config file includes:
+        • All available themes (default, ocean, forest, sunset, monochrome)
+        • Light and dark mode options
+        • Keyboard shortcuts documentation
+        • Customization guide
+
+      To customize your theme:
+        1. Run t-meter once to generate the config file
+        2. Edit ~/.config/t-meter/config.toml
+        3. Change 'theme_name' and 'theme_mode' to your preference
+        4. Restart t-meter
+        5. Press 't' to cycle themes or 'd' to toggle dark mode while running
+    EOS
   end
 
   test do
